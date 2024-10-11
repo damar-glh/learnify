65a81da6169ba7807b63ea0d9b1d9ad8c75c713a
Damar Galih <damarmuhamad13@gmail.com>
2024-10-11 21:48:36 +0700

    Initial commit for Learnify project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#8a70d6',
+        'secondary': '#e9e3ff',
+        'brown': '#424040',
+        'white': '#fbfbfb'
+      },
+    },
+  },
+  plugins: [],
+}
+
