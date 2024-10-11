2245ca9fcdf56e6cc460bff3c9d01375227f557f
Damar Galih <damarmuhamad13@gmail.com>
2024-10-11 23:58:33 +0700

    feat: navbar for dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        'primary': '#8a70d6',
-        'secondary': '#e9e3ff',
-        'brown': '#424040',
-        'white': '#fbfbfb'
-      },
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Roboto', 'sans-serif'],
+            },
+            colors: {
+                'color-pallet': {
+                    'primary': '#F9F9F9',
+                    'secondary': '#F5F5F5',
+                    'tertiary': '#E0E0E0',
+                    'quaternary': '#BDBDBD',
+                    'quinary': '#828282',
+                    'senary': '#4F4F4F',
+                    'septenary': '#333333',
+                    'octonary': '#1A1A1A',
+                    'nonary': '#121212',
+                    'denary': '#000000',
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
