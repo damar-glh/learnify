08f66af389e0fa2cfe7e5751f2a580b3e12f00ef
Damar Galih <damarmuhamad13@gmail.com>
2024-10-12 20:04:22 +0700

    ref: dashboard & course page

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,11 @@ const sidebar = document.getElementById('sidebar');
 const minimizeBtn = document.getElementById('minimizeBtn');
 const leftArrow = document.getElementById('leftArrow');
 const rightArrow = document.getElementById('rightArrow');
-const dashboardContainer = document.querySelector('.dashboard-container')
+const dashboardContainer = document.querySelector('.dashboard-container');
+const courseContainer = document.querySelector('.course-container');
+const profileContainer = document.querySelector('.profile-container');
+const courseBtn = document.getElementById('courseBtn');
+const dashboardBtn = document.getElementById('dashboardBtn');
 
 minimizeBtn.addEventListener('click', function () {
     sidebar.classList.toggle('lg:w-20');
@@ -11,20 +15,13 @@ minimizeBtn.addEventListener('click', function () {
     spans.forEach(span => span.classList.toggle('hidden'));
     leftArrow.classList.toggle('hidden');
     rightArrow.classList.toggle('hidden');
-    if (dashboardContainer.classList.contains('lg:w-2/3')) {
-        dashboardContainer.classList.remove('lg:w-2/3');
-        dashboardContainer.classList.add('w-full');
-    } else {
-        dashboardContainer.classList.remove('lg:w-2/3');
-        dashboardContainer.classList.add('w-2/3');
-    }
 });
 
 document.addEventListener('DOMContentLoaded', () => {
     const menuItems = document.querySelectorAll('.menu-items');
 
     menuItems.forEach(item => {
-        item.addEventListener('click', function() {
+        item.addEventListener('click', function () {
             // Remove the active state from all items
             menuItems.forEach(i => {
                 i.classList.remove('text-color-pallet-denary');
@@ -36,4 +33,14 @@ document.addEventListener('DOMContentLoaded', () => {
             this.classList.add('text-color-pallet-denary');
         });
     });
+});
+
+courseBtn.addEventListener('click', function () {
+    dashboardContainer.classList.add('hidden');
+    courseContainer.classList.remove('hidden');
+});
+
+dashboardBtn.addEventListener('click', function () {
+    courseContainer.classList.add('hidden');
+    dashboardContainer.classList.remove('hidden');
 });
