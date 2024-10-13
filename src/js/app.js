e181cf73f360bfdda7c64e6de77b0b0881734a0c
Damar Galih <damarmuhamad13@gmail.com>
2024-10-13 10:20:56 +0700

    ref: responsive page dashboard and course

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,25 @@ const sidebar = document.getElementById('sidebar');
 const burgerBtn = document.getElementById('burgerBtn');
 const dashboardContainer = document.querySelector('.dashboard-container');
 const courseContainer = document.querySelector('.course-container');
+const closeBtn = document.getElementById('closeBtn');
 const courseBtn = document.getElementById('courseBtn');
 const dashboardBtn = document.getElementById('dashboardBtn');
+const overlay = document.getElementById('overlay');
 
 burgerBtn.addEventListener('click', function () {
     sidebar.classList.toggle('hidden');
     sidebar.classList.toggle('block');
-    const spans = sidebar.querySelectorAll('span');
-    spans.forEach(span => span.classList.toggle('hidden'));
+    if (overlay.classList.contains('hidden')) {
+        overlay.classList.remove('hidden');
+    } else {
+        overlay.classList.add('hidden');
+    }
+});
+
+closeBtn.addEventListener('click', function () {
+    sidebar.classList.add('hidden');
+    sidebar.classList.remove('block');
+    overlay.classList.add('hidden');
 });
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -17,13 +28,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     menuItems.forEach(item => {
         item.addEventListener('click', function () {
-            // Remove the active state from all items
             menuItems.forEach(i => {
                 i.classList.remove('text-color-pallet-denary');
                 i.classList.add('text-color-pallet-quinary');
             });
-
-            // Add the active state to the clicked item
             this.classList.remove('text-color-pallet-quinary');
             this.classList.add('text-color-pallet-denary');
         });
